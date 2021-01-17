581910f7210aa9771122f70b76f76e89a6bbacc4
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-01-17 22:24:30 +0200

    add helper to get fonts loaded (old way)

---
+++ helpers/fetchFonts.js
@@ -0,0 +1,9 @@
+import * as Font from "expo-font";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("../assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("../assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+export default fetchFonts;
