146cd0117ce9f0e620dc4b4c616ee95b56381ec0
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-01-17 22:25:52 +0200

    use two ways to get fonts within react native &(use the modern way)

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+// import AppLoading from "expo-app-loading";
 import { Header } from "./components";
 import StartScreen from "./screens/StartScreen";
 import GameScreen from "./screens/GameScreen";
 import EndScreen from "./screens/EndScreen";
+import { useFonts } from "expo-font";
+
+// import fetchFonts from "./helpers/fetchFonts";
 
 export default function App() {
   const [userChoice, setUserChoice] = React.useState();
   const [roundsNumber, setRoundsNumber] = React.useState(0);
+  // const [fontLoaded, setFontLoaded] = React.useState(false);
+  const [loaded, error] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+  console.log(loaded, "just test");
+  if (!loaded) return null; // maybe return any splash screen
+
+  // if (!fontLoaded) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={fetchFonts}
+  //       onFinish={() => setFontLoaded(true)}
+  //       onError={(err) => console.log(err)}
+  //     />
+  //   );
+  // }
 
   const configureNewGame = () => {
     setUserChoice(null);
